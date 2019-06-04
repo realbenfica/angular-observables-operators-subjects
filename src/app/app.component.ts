a5951ad1fad26566d5dcea691fe42aef699f2fd6
Alex Severino <alex@7lab.nl>
2019-06-04 14:40:10 +0200

    Angular practice

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+    activated = false;
+    private activatedSub: Subscription;
+
+    constructor(
+        private userService: UserService
+    ) {}
+
+    ngOnInit() {
+        this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
+            this.activated = didActivate;
+        });
+    }
+
+    ngOnDestroy() {
+        this.activatedSub.unsubscribe();
+    }
+
+}
